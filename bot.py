9dc5968cfea7bf7a498aaa05fbd8eca4ed9efda6
AniShin21 <minecraft2727k@gmail.com>
2024-08-02 20:11:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
-from plugins.handlers import app  # Import the Client instance with handlers
+from pyrogram import Client
+
+app = Client("my_bot", api_id="your_api_id", api_hash="your_api_hash", bot_token="your_bot_token")
 
 if __name__ == "__main__":
+    # Set up webhook or run bot
     app.run()
-    while True:
-        check_membership_expiration()  # Make sure to move this function to the right place
-        time.sleep(3600)
