303006a95e48a3104faf7de4b0c4f067a68935f8
AniShin21 <minecraft2727k@gmail.com>
2024-08-02 20:12:19 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,39 +1,17 @@
 # database/database.py
-import sqlite3
 
-def connect_db():
-    conn = sqlite3.connect('bot_database.db')
-    return conn
+import os
 
-def initialize_db():
-    conn = connect_db()
-    cursor = conn.cursor()
-    # Create tables if not exists
-    cursor.execute('''CREATE TABLE IF NOT EXISTS premium_users (
-                      user_id INTEGER PRIMARY KEY,
-                      expiration_date TEXT)''')
-    conn.commit()
-    conn.close()
+admins_file = "admins.txt"
 
-def add_premium_user(user_id, expiration_date):
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute('INSERT OR REPLACE INTO premium_users (user_id, expiration_date) VALUES (?, ?)',
-                   (user_id, expiration_date.strftime('%Y-%m-%d %H:%M:%S')))
-    conn.commit()
-    conn.close()
+def load_admin_ids():
+    admin_ids = set()
+    if os.path.exists(admins_file):
+        with open(admins_file, "r") as f:
+            admin_ids.update(int(line.strip()) for line in f.readlines())
+    return admin_ids
 
-def remove_premium_user(user_id):
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute('DELETE FROM premium_users WHERE user_id = ?', (user_id,))
-    conn.commit()
-    conn.close()
-
-def get_premium_users():
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM premium_users')
-    users = cursor.fetchall()
-    conn.close()
-    return users
+def save_admin_ids(admin_ids):
+    with open(admins_file, "w") as f:
+        for admin_id in admin_ids:
+            f.write(str(admin_id) + "\n")
