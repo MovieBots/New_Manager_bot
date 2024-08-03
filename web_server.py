139a2e0e9a9d46c7717dd6f61ab2233bb67b82d8
AniShin21 <minecraft2727k@gmail.com>
2024-08-03 19:51:11 +0530

    Update web_server.py

--- web_server.py
+++ web_server.py
@@ -1,13 +1,9 @@
 from aiohttp import web
 
-async def handle(request):
-    return web.Response(text="I am alive!")
-
 async def web_server():
     app = web.Application()
     app.router.add_get('/', handle)
-    runner = web.AppRunner(app)
-    await runner.setup()
-    site = web.TCPSite(runner, '0.0.0.0', 8080)
-    await site.start()
-    return runner
+    return app
+
+async def handle(request):
+    return web.Response(text="Bot is running")
