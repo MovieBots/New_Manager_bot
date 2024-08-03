e5caa4f69791ae1b0e5e519d96eac53d6a197baf
AniShin21 <minecraft2727k@gmail.com>
2024-08-03 15:57:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,8 +7,8 @@
 # ADMIN_IDS = "123456789 987654321"
 # To know run command contact @iTz_Anayokoji 
 
-API_ID = ""               # Place your API_ID here
-API_HASH = ""             # Place your Api_hash
+API_ID = "15646796"               # Place your API_ID here
+API_HASH = "08bdb932cf2815a46b2a5f17cf245bfe"             # Place your Api_hash
 API_KEY = ""              # Your Tg bot token here 
 OWNER_ID = 6450266465     # Replace with owner id
 ADMIN_IDS = "6450266465"  # Replace With your admins list
