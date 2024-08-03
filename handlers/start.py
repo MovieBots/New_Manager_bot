491bb8195b699fbf58a4665355d8acbaf8f794d7
AniShin21 <minecraft2727k@gmail.com>
2024-08-03 19:52:57 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,10 +1,8 @@
-# handlers/start.py
-
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-def start_command(client: Client, message):
-    client.send_message(
+async def start_command(client: Client, message):
+    await client.send_message(
         message.chat.id,
         "Yo what's up\n\nI am a membership provider bot by Arpit.",
         parse_mode='markdown'
@@ -14,7 +12,7 @@ def start_command(client: Client, message):
          InlineKeyboardButton("Owner", callback_data="owner")]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    client.send_message(
+    await client.send_message(
         message.chat.id,
         "Here are some commands that only admins and owners can use:",
         reply_markup=reply_markup
