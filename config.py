fa5411ed6abb90c1dae599d8c09250d10f4a3fc9
AniShin21 <minecraft2727k@gmail.com>
2024-08-03 06:50:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,10 @@
 # deploy karo vaha lagana varna github account ban ho jata hai '''
 # If you want to put more than one admin then do it like this that is given below
 # ADMIN_IDS = "123456789 987654321"
+# To know run command contact @iTz_Anayokoji 
 
-API_ID = ""
-API_HASH = ""
-API_KEY = ""
-OWNER_ID = 6450266465 # Replace with owner id
-ADMIN_IDS = "6450266465" # Replace With your admins list
+API_ID = ""               # Place your API_ID here
+API_HASH = ""             # Place your Api_hash
+API_KEY = ""              # Your Tg bot token here 
+OWNER_ID = 6450266465     # Replace with owner id
+ADMIN_IDS = "6450266465"  # Replace With your admins list
