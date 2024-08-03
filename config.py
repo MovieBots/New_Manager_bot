1a489dd7fc868bed93d360e88c3a976716dab69a
AniShin21 <minecraft2727k@gmail.com>
2024-08-03 16:32:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,8 @@
-# config.py
-# API_KEY = TG_BOT_TOKEN
-# Port is 8080 for this script 
-# Aprit Bhai tum eak kam karna ke jo hai na API_KEY iss jaha tum 
-# deploy karo vaha lagana varna github account ban ho jata hai '''
-# If you want to put more than one admin then do it like this that is given below
-# ADMIN_IDS = "123456789 987654321"
-# To know run command contact @iTz_Anayokoji 
+import os
 
-API_ID = "15646796"               # Place your API_ID here
-API_HASH = "08bdb932cf2815a46b2a5f17cf245bfe"             # Place your Api_hash
-API_KEY = ""              # Your Tg bot token here 
-OWNER_ID = 6450266465     # Replace with owner id
-ADMIN_IDS = "6450266465"  # Replace With your admins list
+API_ID = os.environ.get("API_ID", "your_api_id")
+API_HASH = os.environ.get("API_HASH", "your_api_hash")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "your_bot_token")
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
+PORT = os.environ.get("PORT", "8080")
+OWNER_ID = int(os.environ.get("OWNER_ID", "6450266465"))
