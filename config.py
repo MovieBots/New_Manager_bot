cd18dcf05a889b7c6a0d65a60bf3d7d5d207b13c
AniShin21 <minecraft2727k@gmail.com>
2024-08-03 06:47:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,11 @@
 # Port is 8080 for this script 
 # Aprit Bhai tum eak kam karna ke jo hai na API_KEY iss jaha tum 
 # deploy karo vaha lagana varna github account ban ho jata hai '''
+# If you want to put more than one admin then do it like this that is given below
+# ADMIN_IDS = "123456789 987654321"
 
-
-API_ID = "15646796"
-API_HASH = "08bdb932cf2815a46b2a5f17cf245bfe"
-API_KEY = "7329302585:AAFfQy7zRD_jnEOBWmddFMNopeqoCetLLP8"
-OWNER_ID = 6450266465
-ADMIN_IDS = "6450266465"
+API_ID = ""
+API_HASH = ""
+API_KEY = ""
+OWNER_ID = 6450266465 # Replace with owner id
+ADMIN_IDS = "6450266465" # Replace With your admins list
