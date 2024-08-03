92ade3e7ee9ceed31b4f97bbf01b06625c49898a
AniShin21 <minecraft2727k@gmail.com>
2024-08-03 17:16:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,25 +1,22 @@
 import logging
-import sys
-from aiohttp import web
+import sys  # Import sys
 from pyrogram import Client, filters
 from pyrogram.enums import ParseMode
-from datetime import datetime
+from datetime import datetime  # Import datetime
 from config import API_HASH, API_ID, TG_BOT_TOKEN, TG_BOT_WORKERS, PORT
-
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
-
 
 class Bot(Client):
     def __init__(self):
         super().__init__(
             name="Bot",
             api_hash=API_HASH,
-            api_id=API_ID,
+            api_id=API_ID,  # Use API_ID instead of APP_ID
             plugins={
-                "root": "plugins"
+                "root": "plugins"  # Ensure this points to the correct plugins directory
             },
             workers=TG_BOT_WORKERS,
             bot_token=TG_BOT_TOKEN
@@ -30,13 +27,10 @@ class Bot(Client):
         await super().start()
         usr_bot_me = await self.get_me()
         self.uptime = datetime.now()
-
-        try:
-            self.set_parse_mode(ParseMode.HTML)
-            self.LOGGER.info(
-                f"Bot Running..!\n\nCreated by \n@iTz_Anayokoji")
-            self.LOGGER.info(f""" \n\n
         
+        self.set_parse_mode(ParseMode.HTML)
+        self.LOGGER.info(f"Bot Running..!\n\nCreated by \n@iTz_Anayokoji")
+        self.LOGGER.info(f"""\n\n
  █████╗ ███╗   ██╗██╗███████╗██╗  ██╗██╗███╗   ██╗
 ██╔══██╗████╗  ██║██║██╔════╝██║  ██║██║████╗  ██║
 ███████║██╔██╗ ██║██║███████╗███████║██║██╔██╗ ██║
@@ -45,22 +39,13 @@ class Bot(Client):
 ╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝╚══════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝                 
                                    
                                     """)
-            self.username = usr_bot_me.username
-
-            # web-response
-            app = web.AppRunner(await web_server())
-            await app.setup()
-            bind_address = "0.0.0.0"
-            await web.TCPSite(app, bind_address, PORT).start()
-        except Exception as e:
-            self.LOGGER.warning(e)
-            self.LOGGER.warning("Failed to start the bot properly.")
-            sys.exit()
+        self.username = usr_bot_me.username
 
     async def stop(self, *args):
         await super().stop()
         self.LOGGER.info("Bot stopped.")
 
-# Initialize and run the bot
-app = Bot()
-app.run()
+if __name__ == "__main__":
+    bot = Bot()
+    bot.run()
+
