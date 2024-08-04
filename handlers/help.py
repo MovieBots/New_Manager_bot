687ad45aa071a3783d6727b5134afc63792b6ac3
AniShin21 <minecraft2727k@gmail.com>
2024-08-04 16:43:41 +0530

    Update help.py

--- handlers/help.py
+++ handlers/help.py
@@ -1,6 +1,7 @@
-from pyrogram import Client, filters
+from pyrogram import Client
 from pyrogram.types import Message
 
 async def help_command(client: Client, message: Message):
     await message.reply("Here is how to use me:\nUse /buy_premium to buy membership.\nIf you want to know more, contact Owner.")
+
 
