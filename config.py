01a352c038aa64b9a7cb03b3a8be68721b478b2f
AniShin21 <minecraft2727k@gmail.com>
2024-08-04 12:13:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,4 +10,7 @@ TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 PORT = os.environ.get("PORT", "8080")
 OWNER_ID = int(os.environ.get("OWNER_ID", "6450266465"))
-ADMINS = [int(admin) for admin in os.getenv("ADMINS", "").split()]
+ADMINS = [int(admin) for admin in os.getenv("ADMINS", "6450266465").split()]
+
+if not API_ID or not API_HASH or not TG_BOT_TOKEN:
+    raise ValueError("Missing API_ID, API_HASH, or TG_BOT_TOKEN in environment variables.")
