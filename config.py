4505caeb5112ad814a0b0be4ea883f98247bcc26
MovieBots <154045205+MovieBots@users.noreply.github.com>
2024-08-04 16:15:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-API_ID = os.environ.get("API_ID", "15646796")
-API_HASH = os.environ.get("API_HASH", "08bdb932cf2815a46b2a5f17cf245bfe")
+API_ID = os.environ.get("API_ID", "29236719")
+API_HASH = os.environ.get("API_HASH", "1ccf1bd0a86af974e3210a55f662c062")
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 PORT = os.environ.get("PORT", "8080")
-OWNER_ID = int(os.environ.get("OWNER_ID", "6450266465"))
-ADMINS = [int(admin) for admin in os.getenv("ADMINS", "6450266465").split()]
+OWNER_ID = int(os.environ.get("OWNER_ID", "893383574"))
+ADMINS = [int(admin) for admin in os.getenv("ADMINS", "893383574").split()]
 
 if not API_ID or not API_HASH or not TG_BOT_TOKEN:
     raise ValueError("Missing API_ID, API_HASH, or TG_BOT_TOKEN in environment variables.")
