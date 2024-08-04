2ca8e6bdc9be4a2b231b4ff6fd80b278aacff90a
AniShin21 <minecraft2727k@gmail.com>
2024-08-04 16:44:43 +0530

    Update user_stats.py

--- handlers/user_stats.py
+++ handlers/user_stats.py
@@ -1,4 +1,4 @@
-from pyrogram import Client, filters
+from pyrogram import Client
 from pyrogram.types import CallbackQuery
 
 async def user_details_callback(client: Client, callback_query: CallbackQuery):
@@ -7,5 +7,4 @@ async def user_details_callback(client: Client, callback_query: CallbackQuery):
     details = (f"Name: {user.first_name} {user.last_name or ''}\n"
                f"Username: @{user.username or 'N/A'}\n"
                f"ID: {user.id}\n"
-               f"Expiration Date: {premium_users[user_id].strftime('%Y-%m-%d')}")
-    await client.send_message(callback_query.message.chat.id, details)
+               f"Expiration Date: {premium_users[user_id].strftime('%Y-%m-%d
