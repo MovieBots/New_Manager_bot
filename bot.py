7962a047662d9c2a9b8265d61e358bfbcb2d8691
AniShin21 <minecraft2727k@gmail.com>
2024-08-04 15:02:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from pyrogram.handlers import MessageHandler, CallbackQueryHandler
 from datetime import datetime
 import logging
 from config import API_ID, API_HASH, TG_BOT_TOKEN, TG_BOT_WORKERS, OWNER_ID, ADMINS
+import asyncio
 
 # Import your handlers
 from handlers.start import start_command
@@ -34,6 +35,8 @@ class Bot(Client):
         
         logger.info(f"Bot Running..!\n\nCreated by \n@iTz_Anayokoji")
         logger.info(f"""\n\n
+
+        
  █████╗ ███╗   ██╗██╗███████╗██╗  ██╗██╗███╗   ██╗
 ██╔══██╗████╗  ██║██║██╔════╝██║  ██║██║████╗  ██║
 ███████║██╔██╗ ██║██║███████╗███████║██║██╔██╗ ██║
@@ -43,7 +46,7 @@ class Bot(Client):
                                    
                                     """)
 
-    def run(self):
+    async def run(self):
         # Register handlers
         self.add_handler(MessageHandler(start_command, filters.command("start")))
         self.add_handler(MessageHandler(handle_admin_commands, filters.private & filters.text))
@@ -53,13 +56,13 @@ class Bot(Client):
         self.add_handler(CallbackQueryHandler(user_details_callback, filters.regex(r"user_details_\d+")))
 
         # Start the bot
-        self.start()
+        await self.start()
         # Run the bot's idle loop
-        self.idle()
+        await self.idle()
 
 if __name__ == "__main__":
     bot = Bot()
-    bot.run()
-
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(bot.run())
 
 
