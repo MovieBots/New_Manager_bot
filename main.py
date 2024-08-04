c4ef166ac1e4161d03fdb097d0b8b6b46e4fc0f8
AniShin21 <minecraft2727k@gmail.com>
2024-08-04 16:07:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-# Dont Change anythink it will lead to an error
-
-import asyncio
-
 from bot import Bot
 
-async def main():
-    bot = Bot()
-    await bot.run()
-
 if __name__ == "__main__":
-    asyncio.run(main())
+    bot = Bot()
+    bot.run()
